9803939396e03903b4fe139571a586dc69deced7
javdevV <houssem.missaoui@esprit.tn>
2019-12-17 00:14:48 +0100

    updated : new css

--- public/js/app.js
+++ public/js/app.js
@@ -49488,10 +49488,10 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/myStyle.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/myStyle.scss ***!
+  \*************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -49500,14 +49500,14 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/myStyle.scss ***!
+  \*****************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! D:\Dev\personal\laravel\test\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Dev\personal\laravel\test\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\Dev\personal\laravel\test\resources\sass\myStyle.scss */"./resources/sass/myStyle.scss");
 
 
 /***/ })
